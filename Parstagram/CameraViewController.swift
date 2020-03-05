09ef8a473e97296a41a3c5a5f7da9e4ada054576
Maya Whaley <mawhaley430@gmail.com>
2020-03-04 21:37:45 -0500

    completed assignment.

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
@@ -17,9 +18,29 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
 	@IBOutlet weak var commentField: UITextField!
 	
 	@IBAction func submitButton(_ sender: Any) {
+		let posts = PFObject(className: "Posts")
+		
+		posts["caption"] = commentField.text!
+		posts["author"] = PFUser.current()!
+		
+		let imageData = imageView.image!.pngData()
+		let file = PFFileObject(name: "image.png", data: imageData!)
+//		let file = PFFileObject(data: imageData!)
+		
+		posts["image"] = file
+		
+		posts.saveInBackground { (success, error) in
+			if success{
+				print("saved!")
+				self.dismiss(animated: true, completion: nil)
+			}else{
+				print("error!")
+			}
 	}
-	
+	}
 	@IBAction func onCameraButton(_ sender: Any) {
+		
+		
 		let picker = UIImagePickerController()
 		picker.delegate = self
 		picker.allowsEditing = true
@@ -42,12 +63,12 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
 		
 		dismiss(animated: true, completion: nil)
 	}
+	
 	override func viewDidLoad() {
-        super.viewDidLoad()
+		super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+		// Do any additional setup after loading the view.
+	}
 
     /*
     // MARK: - Navigation
